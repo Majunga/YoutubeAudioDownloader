cf419cc71de133bd49d7b267f166b6340bb952e5
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2023-04-12 17:17:28 +0100

    You can now run the exe and it will ask you for the url
    You can enter urls without the https:// and it will correct it
    
    Renamed, since I might want to do video too :P

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,27 @@
 ﻿using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
 
+var originalUrlString = string.Empty;
 if (args.Length == 0)
 {
-    Console.WriteLine("Youtube url required");
-    return 1;
+    Console.Write("Enter Url:");
+    originalUrlString = Console.ReadLine();
+}
+else if (originalUrlString == string.Empty && args.Length > 0)
+{
+    originalUrlString = args[0];
 }
 
-var urlString = args[0];
-if(Uri.TryCreate(urlString, UriKind.Absolute, out var url) == false)
+var urlString = originalUrlString ?? string.Empty;
+
+if(urlString.StartsWith("https://") == false && urlString.StartsWith("http://") == false)
 {
-    Console.WriteLine($"Invalid Url: {urlString}");
+    urlString = $"https://{urlString}";
+}
+
+if (Uri.TryCreate(urlString, UriKind.Absolute, out var url) == false)
+{
+    Console.WriteLine($"Invalid Url: {originalUrlString}");
     return 1;
 }
 
@@ -20,6 +31,7 @@ var video = await youtoobs.Videos.GetAsync(urlString);
 var streamManifest = await youtoobs.Videos.Streams.GetManifestAsync(video.Id);
 var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
 
+Console.WriteLine("Downloading Audio");
 await youtoobs.Videos.Streams.DownloadAsync(streamInfo, $"{Environment.CurrentDirectory}/{video.Id}.{streamInfo.Container.Name}");
-
+Console.WriteLine("Done!");
 return 0;
